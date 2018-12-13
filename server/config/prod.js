86b207172535475f98ff0d52761f1f4c14e65add
DannyVaughan1990 <danieljavaughan@outlook.com>
2018-12-13 14:08:51 +0000

    Finish auth flow

---
+++ server/config/prod.js
@@ -0,0 +1,7 @@
+// prod.js - production keys here (Commit this file)
+module.exports = {
+    googleClientID:  proccess.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: proccess.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: proccess.env.MONGO_URI,
+    cookieKey: proccess.env.COOKIE_KEY 
+};
