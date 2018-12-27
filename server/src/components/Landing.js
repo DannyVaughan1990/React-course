35221ef862fa5740e2df2e5c40b051e2bb33cd5b
Daniel Vaughan <36799925+DannyVaughan1990@users.noreply.github.com>
2018-12-27 21:57:52 +0000

    Add files via upload

---
+++ server/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1>
+                Daniel Vaughan Feedback App!
+            </h1>
+            Collect feedback from your clients and customers.
+        </div>
+    );
+};
+
+export default Landing;
