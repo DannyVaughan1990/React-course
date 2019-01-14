8eb28479895bb58a61597223b7244c5be4dc9d49
Daniel Vaughan <36799925+DannyVaughan1990@users.noreply.github.com>
2019-01-14 18:26:59 +0000

    Add files via upload

---
+++ server/src/components/surveys/SurveyForm.js
@@ -0,0 +1,59 @@
+// SurveyForm shows a form for a user to add input
+import _ from 'lodash';
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
+import SurveyField from './SurveyField';
+
+const FIELDS = [
+    {label: 'Suvery Title', name: 'title', noValueError: 'Please provide a title'},
+    {label: 'Subject Line', name: 'subject', noValueError: 'Please provide a subject'},
+    {label: 'Email Body', name: 'body', noValueError: 'Please provide a body'},
+    {label: 'Recipient List', name: 'emails', noValueError: 'Please provide a recipient/ recipients (comma seperated list)'}
+];
+
+class SurveyForm extends Component {
+    renderFields() {
+       return _.map(FIELDS, ({ label, name }) => {
+           return (
+                <Field key={name} component={SurveyField} type="text" label={label} name={name} />
+         );
+      });
+    }
+
+
+    render() {
+        return (
+            <div> 
+                <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+                  {this.renderFields()}
+                  <Link to="/surveys" className="red btn-flat white-text">
+                  Cancel
+                  </Link>
+                   <button type="submit" className="red btn-flat right black-text">
+                   Next
+                   <i className="material-icons right">done</i>
+                   </button>
+                </form>
+            </div>
+        );
+    }
+}
+
+function validate(values) {
+    const errors ={};
+
+    _.each(FIELDS, ({ name, noValueError }) => {
+        if (!values[name]){
+            errors[name] = noValueError;
+        }
+    });
+
+
+    return errors;
+}
+
+export default reduxForm({
+    validate,
+    form: 'surveyForm'
+})(SurveyForm); 
