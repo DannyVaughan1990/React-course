29419e99cfd065a79d5134dd7086b5ed4d63df5b
Daniel Vaughan <36799925+DannyVaughan1990@users.noreply.github.com>
2019-01-15 05:06:24 +0000

    Add files via upload
    Add survey review

--- server/src/components/surveys/SurveyForm.js
+++ server/src/components/surveys/SurveyForm.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
+import validateEmails from '../../utils/validateEmails';
 
 const FIELDS = [
     {label: 'Suvery Title', name: 'title', noValueError: 'Please provide a title'},
@@ -25,7 +26,7 @@ class SurveyForm extends Component {
     render() {
         return (
             <div> 
-                <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+                <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
                   {this.renderFields()}
                   <Link to="/surveys" className="red btn-flat white-text">
                   Cancel
@@ -42,6 +43,8 @@ class SurveyForm extends Component {
 
 function validate(values) {
     const errors ={};
+
+    errors.emails = validateEmails(values.emails || '');
 
     _.each(FIELDS, ({ name, noValueError }) => {
         if (!values[name]){
--- server/src/components/surveys/SurveyNew.js
+++ server/src/components/surveys/SurveyNew.js
@@ -1,12 +1,25 @@
 // SurveyNew shows survey form and survey review
 import React, { Component } from 'react';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
+    state = { showFormReview: false };
+
+    renderContent() {
+        if (this.state.showFormReview) {
+            return <SurveyFormReview />;
+        }
+
+        return <SurveyForm 
+        onSurveySubmit={() => this.setState({ showFormReview: true })}
+        />;
+    }
+
     render() {
         return (
             <div>
-                <SurveyForm />
+                {this.renderContent()}
             </div>
         );
     }
